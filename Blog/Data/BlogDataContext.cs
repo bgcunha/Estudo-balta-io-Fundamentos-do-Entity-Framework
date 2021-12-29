28ad5d25036550cf8e1e44a3c9646e1cad91307d
Bruno Gomes <brunnu.cunha@gmail.com>
2021-12-29 12:13:26 -0300

    Mapeamento

--- Blog/Data/BlogDataContext.cs
+++ Blog/Data/BlogDataContext.cs
@@ -1,20 +1,17 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Blog.Models;
 
 namespace Blog.Data
 {
-    public class BlogDataContext : DbContext
+    class BlogDataContext : DbContext
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
-        // public DbSet<PostTag> PostTags { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<User> Users { get; set; }
-        // public DbSet<UserRole> UserRoles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Server=DESKTOP-BKLEGIN\\SQLEXPRESS2019,Database=Blog;User ID=sa;Password=sa123");
-
+            => options.UseSqlServer("Server=DESKTOP-BKLEGIN\\SQLEXPRESS2019; Database=Blog;User ID=sa;Password=sa123");
     }
 }
--- blog/Program.cs
+++ blog/Program.cs
@@ -1,12 +1,76 @@
 ﻿using System;
+using System.Linq;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace blog
+namespace Blog
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //try
+            //{
+                using var context = new BlogDataContext();
+
+                var user = new User
+                {
+                    Name = "Bruno Gomes",
+                    Slug = "brunogomes",
+                    Email = "brunogomes@bruno.io",
+                    Bio = "Anaista Senior",
+                    Image = "https://brunogomes.io",
+                    PasswordHash = "123456789"
+                };
+
+                var category = new Category { Name = "Backend", Slug = "backend" };
+
+                var post = new Post
+                {
+                    Author = user,
+                    Category = category,
+                    Body = "<p>Hello world</p>",
+                    Slug = "comecando-com-ef-core",
+                    Summary = "Neste artigo vamos aprender EF core",
+                    Title = "Começando com EF Core",
+                    CreateDate = DateTime.Now,
+                    LastUpdateDate = DateTime.Now,
+                };
+
+                context.Posts.Add(post); // Não adicionei autor nem categoria
+                context.SaveChanges();
+
+                // var posts = context
+                //     .Posts
+                //     .AsNoTracking()
+                //     .Include(x => x.Author)
+                //     .Include(x => x.Category)
+                //     .OrderBy(x => x.LastUpdateDate)
+                //     .ToList();
+                //
+                // foreach (var post in posts) 
+                //     Console.WriteLine($"{post.Title} por {post.Author.Name} em {post.Category.Name}");
+
+                // var post = context
+                //     .Posts
+                //     // .AsNoTracking() PRECISA DO TRACKING 
+                //     .Include(x => x.Author)
+                //     .Include(x => x.Category)
+                //     .OrderBy(x => x.LastUpdateDate)
+                //     .FirstOrDefault(); // Pegando o primeiro item
+                //
+                // post.Author.Name = "Uncle Bob";
+                //
+                // context.Posts.Update(post);
+                // context.SaveChanges();
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"ERRO>>>:{ex.Message}");
+            //}
+
         }
     }
 }
